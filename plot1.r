f33c1fda2f5231b21dc2c697dbe05d95699cac85
u0115286 <ronald.teo@thomsonreuters.com>
2016-03-19 13:49:22 -0500

    Add code to download data

--- plot1.r
+++ plot1.r
@@ -1,4 +1,7 @@
-
+if (is.na(file.info("household_power_consumption.txt")$size)) {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.zip")
+  unzip("household_power_consumption.zip")
+}
 power <- read.csv("household_power_consumption.txt",sep=";",na.strings = "?", colClasses = "character")
 power$Date <- as.Date(power$Date, "%d/%m/%Y")
 power_sub <- subset(power, power$Date==as.Date("1/2/2007", "%d/%m/%Y") | power$Date==as.Date("2/2/2007", "%d/%m/%Y"))
